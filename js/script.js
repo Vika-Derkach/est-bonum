99e31e4b2fef65e1710f01ba8af3e29f79426b13
Vika-Derkach <queenviktoriyphp@gmail.com>
2022-04-30 13:12:03 +0300

    menu mobile

--- js/script.js
+++ js/script.js
@@ -1,40 +1,43 @@
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  //   direction: "vertical",
   loop: true,
-  // grabCursor: true,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    // currentClass: 'swiper-pagination-current'
   },
   slidesPerView: 2,
-  // slidesPerGroup: 2,
-  // autoHeight: true,
 
-  //   loopedSlides: 2,
-  //   spaceBetween: 10,
-  // using "ratio" endpoints
   breakpoints: {
     "@0.25": {
       slidesPerView: 1,
-      //   spaceBetween: 20,
     },
     "@1.00": {
       slidesPerView: 2,
-      //   spaceBetween: 40,
     },
     "@1.50": {
       slidesPerView: 2,
-      //   spaceBetween: 50,
     },
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-next-button",
     prevEl: ".swiper-prev-button",
   },
 });
+
+const link = document.querySelector(".menu-link");
+const link_active = document.querySelector(".menu-link_active");
+const menu_mobile = document.querySelector(".menu-mobile");
+const nav_link = menu_mobile.querySelectorAll("a");
+
+link.addEventListener("click", () => {
+  link.classList.toggle("menu-link_active");
+  menu_mobile.classList.toggle("menu-mobile_active");
+});
+
+for (let i = 0; i < nav_link.length; i++) {
+  nav_link[i].addEventListener("click", () => {
+    link.classList.toggle("menu-link_active");
+    menu_mobile.classList.toggle("menu-mobile_active");
+  });
+}
